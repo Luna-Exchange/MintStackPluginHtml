37868438a6582558942f68996e13628eba974f2d
versedpro <ultraj0330@gmail.com>
2022-11-18 17:49:04 +0800

    feat: Add simple web3 functionality

--- src/CheckoutWidget.ts
+++ src/CheckoutWidget.ts
@@ -1,3 +1,4 @@
+import { ethers } from "ethers";
 import { LitElement, html } from "lit";
 import { customElement, property } from 'lit/decorators.js';
 import { getMintInfo } from "./api/mint";
@@ -7,10 +8,10 @@ export class CheckoutWidget extends LitElement {
     @property({type: Object})
     mintInfo: any = {}
 
-    connectedCallback(): void {
+    async connectedCallback(): Promise<void> {
         super.connectedCallback();
 
-        const response = getMintInfo('f3e47701-a883-48d7-9e9a-d985792ab12e');
+        const response = await getMintInfo('f3e47701-a883-48d7-9e9a-d985792ab12e');
         this.mintInfo = response;
         console.log(this.mintInfo);
 
@@ -19,7 +20,13 @@ export class CheckoutWidget extends LitElement {
     render() {
         return html`
             <span>First test</span>
+            <button @click=${this.connectWallet}>Connect Wallet</button>
         `;
+    }
+
+    async connectWallet(event: Event) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send('eth_requestAccounts', []);
     }
     
 }
